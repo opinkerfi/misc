5f46f8980d253a9ee314e4537e03dc00022fcd4b
Tómas Edwardsson <tommi@opensource.is>
2011-03-22 11:02:49 +0000

    Added support for host notifications. Added LONG OUTPUT to the email notification

--- misc/perty-nagios-mailer.pl/trunk/perty-nagios-mailer.pl
+++ misc/perty-nagios-mailer.pl/trunk/perty-nagios-mailer.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl 
 #
-# Copyright 2010, Tomas Edwardsson 
+# Copyright 2011, Tomas Edwardsson 
 #
 # This script is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -30,41 +30,123 @@ my $pnp4nagios_url = "http://nagios/nagios/pnp4nagios/";
 my $from_address = 'nagios@opensource.is';
 my $logo = '/usr/share/nagios/html/images/sblogo.png';
 
-if (@ARGV != 8) {
+my $alert_type = 'service';
+my $recipient = '';
+my $date = '';
+my $type = '';
+my $host = '';
+my $hostalias = '';
+my $ip = '';
+my $state = '';
+my $message = '';
+my $servicedesc = '';
+
+
+sub usage() {
+	print STDERR <<"	EOUSAGE";
+$0 [-H] <email> <date_string> <notification_type> <hostname> <host_alias> <host_address> <state> <notification_string> [servicedesc]
+
+    -H			Host notification, skip servicedesc
+
+    email			Recipient of notification
+    date_string		eg, Fri Oct 13 00:30:28 CDT 2000
+    notification_type	One of the following types:
+		PROBLEM, RECOVERY, ACKNOWLEDGEMENT, FLAPPINGSTART, FLAPPINGSTOP,
+		FLAPPINGDISABLED, DOWNTIMESTART, DOWNTIMEEND or
+		DOWNTIMECANCELLED
+    hostname		Hostname of affected device
+    host_alias		Hostname alias, descriptive
+    host_address		IP Address of device
+    state			State of affected device
+	host		UP, DOWN or UNREACHABLE
+	service		OK, WARNING, UNKNOWN or CRITICAL
+    notification_string	The description of what went wrong
+    servicedesc		Service Description of device
+
+	EOUSAGE
+
+	exit 3;
+}
+
+my @cleansed_argv = ();
+# Look for service or host options, default to service
+foreach my $a (@ARGV) {
+	if ($a eq '-H' or $a eq '--host') {
+		$alert_type = 'host';
+	} else {
+		push @cleansed_argv, $a;
+	}
+}
+
+
+if (($alert_type eq 'service' and @cleansed_argv != 9) or ($alert_type eq 'host'
+	and @cleansed_argv != 8)) {
 	usage();
 }
 
-my ($recipient, $date, $type, $host, $ip, $service, $state, $message) = @ARGV;
 
+# Assign variable from cmd line
+if ($alert_type eq 'service') {
+	($recipient, $date, $type, $host, $hostalias, $ip, $state, $message, $servicedesc) = @cleansed_argv;
+} else {
+	($recipient, $date, $type, $host, $hostalias, $ip, $state, $message) = @cleansed_argv;
+}
+
+
+# Nagios Command int, see nagios src, nagios/include/common.h
 my %nagioscmd = (
+	host_downtime => 55,
+	host_ack => 33,
 	service_downtime => 56,
 	service_ack => 34,
 );
 
+
+
+# Locate graph for machine and attach to message
+# Based on pnp4nagios v0.4.X not tested with v0.6.X
+
 my $rrd = '';
-if (-f "$pnp4nagios_perfdata/$host/$service.rrd") {
+
+
+# Get the specified service graph
+if ($alert_type eq 'service' and -f "$pnp4nagios_perfdata/$host/$servicedesc.rrd") {
 	my $t = time();
-	$rrd = `( cd $pnp4nagios_phpdir;php -r 'parse_str("host=$host&srv=$service&source=1&view=0&end=$t&display=image", \$_GET); include_once("index.php");' )`;
+	$rrd = `( cd $pnp4nagios_phpdir;php -r 'parse_str("host=$host&srv=$servicedesc&source=1&view=0&end=$t&display=image", \$_GET); include_once("index.php");' )`;
+# Get the specified HOST graph
+} elsif ($alert_type eq 'host' and -f "$pnp4nagios_perfdata/$host/_HOST_.rrd") {
+	my $t = time();
+	$rrd = `( cd $pnp4nagios_phpdir;php -r 'parse_str("host=$host&srv=_HOST_&source=1&view=0&end=$t&display=image", \$_GET); include_once("index.php");' )`;
 }
 
+
+# Now we build the message
+# MESSAGE Structure
+#	HEAD PART multipart/related
+#		ALT PART multipart/alternative
+#			text content part text/plain
+#			html_content part text/html
+#		RRD IMAGE PART image/png
+#		LOGO IMAGE PART image/png <sblogo.png>
+
+# The real envelope (TOP PART)
 my $head = MIME::Entity->build(
 	Type		=> 'multipart/related',
 	From		=> $from_address,
 	To		=> $recipient,
-	Subject		=> "Nagios, $state - $host - $service"
+	Subject		=> "Nagios, $state - $host" . ($servicedesc ? " - $servicedesc" : ""),
 );
 
+# Alternative part for text and html content
 my $alt = MIME::Entity->build(
 	Type		=> 'multipart/alternative'
 );
-
 my $txt_content = MIME::Entity->build(
 	Type		=> 'text/plain',
 	Charset		=> 'UTF-8',
 	Encoding	=> 'quoted-printable',
-	Data		=> sprintf(<<EO),
-Host: $host ($ip)
-Service: $service
+	Data		=> sprintf(<<EO, ($servicedesc ? "\nService: $servicedesc" : "")),
+Host: $host ($ip)%s
 State: $state
 Date: $date
 Notification Type: $type
@@ -74,23 +156,29 @@ EO
 
 my $state_color = '#33FF00';
 
-if ($state eq 'WARNING') {
+if ($state eq 'WARNING' or $state eq 'UNREACHABLE') {
 	$state_color = '#FFFF00';
-} elsif ($state eq 'CRITICAL') {
+} elsif ($state eq 'CRITICAL' or $state eq 'DOWN') {
 	$state_color = '#F83838';
 }
 
 
 my $ack = '';
-if ($state ne 'OK') {
-	my $ackurl = sprintf("%s/cmd.cgi?host=%s&service=%s",
+if ($state ne 'OK' and $state ne 'UP') {
+	my $ackurl = sprintf("%s/cmd.cgi?host=%s%s",
 		$nagios_cgiurl,
 		$host,
-		$service);
+		($servicedesc ? "&service=$servicedesc" : ""));
 	$ack = qq{
   <tr>
     <td colspan="1" style="background: black;color: white;font-weight: bold">Service Actions</td>
-    <td colspan="1" style="background: white;color: black"><a class="cmd" href="$ackurl&cmd_typ=$nagioscmd{service_ack}">Acknowledge</a> <a class="cmd" href="$ackurl&cmd_typ=$nagioscmd{service_downtime}">Schedule Downtime</a></td>
+    <td colspan="1" style="background: white;color: black"><a class="cmd" href="$ackurl&cmd_typ=} . 
+	# Service or host ack
+	($alert_type eq 'service' ? $nagioscmd{service_ack} : $nagioscmd{host_ack}) . 
+	qq{">Acknowledge</a> <a class="cmd" href="$ackurl&cmd_typ=} . 
+	# Service or host downtime
+	($alert_type eq 'service' ? $nagioscmd{service_downtime} : $nagioscmd{host_downtime}) . 
+	qq{">Schedule Downtime</a></td>
   </tr>
 };
 }
@@ -132,11 +220,12 @@ a.cmd {
   <tr>
     <th>Host</td>
     <td style="background: white;color: black"><a style="color: black;text-decoration: underline" href="$nagios_cgiurl/extinfo.cgi?type=1&host=$host">$host ($ip)</a></td>
-  </tr>
+  </tr>} .
+	($alert_type eq 'host' ? "" : qq{
   <tr>
     <th>Service</td>
-    <td style="background: white;color: black"><a style="color: black;text-decoration: underline" href="$nagios_cgiurl/extinfo.cgi?type=2&host=$host&service=$service">$service</a></td>
-  </tr>
+    <td style="background: white;color: black"><a style="color: black;text-decoration: underline" href="$nagios_cgiurl/extinfo.cgi?type=2&host=$host&service=$servicedesc">$servicedesc</a></td>
+  </tr>}) . qq{
   <tr>
     <th>State</td>
     <td style="background: $state_color;color: black;font-weight: bold">$state</td>
@@ -159,7 +248,7 @@ $ack
 } . ($rrd ? qq{
   <tr>
     <td style="background: white" colspan="2">
-      <a href="$pnp4nagios_url?host=$host&srv=$service"><img border=0 src="cid:pnp.png"></a>
+      <a href="$pnp4nagios_url?host=$host} . ($servicedesc ? "&srv=$servicedesc" : "") . qq{"><img border=0 src="cid:pnp.png"></a>
     </td>
   </tr>
 } : "") . qq{
@@ -190,5 +279,6 @@ $head->attach(
 open SENDMAIL, '|/usr/sbin/sendmail -t';
 $head->print(\*SENDMAIL);
 close SENDMAIL;
+
 
 

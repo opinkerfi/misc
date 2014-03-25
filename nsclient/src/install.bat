798226848dbdec38a9efb099badb6544bfce4d7f
Pall Sigurdsson <palli@opensource.is>
2014-03-25 17:43:55 +0000

    nsclient - install.bat updated

--- nsclient/src/install.bat
+++ nsclient/src/install.bat
@@ -5,10 +5,12 @@ net stop NSClientpp
 xcopy "%ProgramFiles%\NSclient++\*.ini" "%ProgramFiles%\NSclient++\backup\"  /i /h /y 
 
 xcopy %0\..\%PROCESSOR_ARCHITECTURE%\*.*  "%ProgramFiles%\NSclient++\" /e /i /h /y
+xcopy "%0\..\tmp\nscp-%PROCESSOR_ARCHITECTURE%.msi"  "%TEMP%\" /e /i /h /y
+msiexec /quiet /i "%TEMP%\nscp-%PROCESSOR_ARCHITECTURE%.msi" CONF_NRPE=1
 xcopy %0\..\datafiles\*.*  "%ProgramFiles%\NSclient++\" /e /i /h /y
 
 
-"%ProgramFiles%\NSclient++\nsclient++.exe" -uninstall
-"%ProgramFiles%\NSclient++\nsclient++.exe" -install
+"%ProgramFiles%\NSclient++\nscp.exe" service --uninstall
+"%ProgramFiles%\NSclient++\nscp.exe" service --install
+"%ProgramFiles%\NSclient++\nscp.exe" service --start
 
-Net Start NSClientpp
